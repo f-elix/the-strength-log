04f9616f9fe63b4481a04f502652347f890248f1
Felix <felix_guerin@hotmail.fr>
2019-12-24 14:30:54 -0500

    Prepare server file for deployement

--- server.js
+++ server.js
@@ -17,39 +17,39 @@ const mongoose = require("mongoose");
 
 // Get current user on every request
 const getUser = async token => {
-  if (token) {
-    try {
-      return await jwt.verify(token, process.env.SECRET);
-    } catch (err) {
-      throw new AuthenticationError("Your session has ended. Please sign in again.");
-    }
-  }
+	if (token) {
+		try {
+			return await jwt.verify(token, process.env.SECRET);
+		} catch (err) {
+			throw new AuthenticationError("Your session has ended. Please sign in again.");
+		}
+	}
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    const token = req.headers.authorization || "";
-    const currentUser = await getUser(token);
-    return {
-      currentUser
-    };
-  }
+	typeDefs,
+	resolvers,
+	context: async ({ req }) => {
+		const token = req.headers.authorization || "";
+		const currentUser = await getUser(token);
+		return {
+			currentUser
+		};
+	}
 });
 
 mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => {
-    console.log("DB connected");
-    return server.listen();
-  })
-  .then(({ url }) => {
-    console.log("Server listening on " + url);
-  })
-  .catch(err => console.log(err));
+	.connect(process.env.DB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then(() => {
+		console.log("DB connected");
+		return server.listen({ port: process.env.PORT || 4000 });
+	})
+	.then(({ url }) => {
+		console.log("Server listening on " + url);
+	})
+	.catch(err => console.log(err));
