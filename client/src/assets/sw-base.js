3e23d26a59110097b7b7365da00e709c001b32eb
Felix <felix_guerin@hotmail.fr>
2020-01-27 14:42:02 -0500

    Cache google fonts

--- client/src/assets/sw-base.js
+++ client/src/assets/sw-base.js
@@ -2,6 +2,20 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 workbox.core.setCacheNameDetails({ prefix: "the-strength-log" });
 
+// Cache Google fonts:
+workbox.routing.registerRoute(
+	new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+	workbox.strategies.cacheFirst({
+		cacheName: "googleapis",
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 30,
+				maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+			})
+		]
+	})
+);
+
 self.addEventListener("message", event => {
 	if (event.data && event.data.type === "SKIP_WAITING") {
 		self.skipWaiting();
