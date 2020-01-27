a24ebe70d89e04d3bc72c90fc4171c738eeebff1
Felix <felix_guerin@hotmail.fr>
2020-01-27 18:27:10 -0500

    test

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -26,12 +26,10 @@ if (process.env.NODE_ENV === "production") {
 			const dismissButton = document.getElementById("dismiss-button");
 
 			updateBanner.style.display = "block";
-
 			updateButton.addEventListener("click", () => {
 				registration.waiting.postMessage({ type: "SKIP_WAITING" });
 				window.location.reload();
 			});
-
 			dismissButton.addEventListener("click", () => {
 				updateBanner.style.display = "none";
 			});
