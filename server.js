71626c7ddfb996b13ed5a1750e74b89880ce1a10
Felix <felix_guerin@hotmail.fr>
2020-02-14 23:19:29 -0500

    Revert to Apollo

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 // Packages
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 // Apollo imports
-const { ApolloServer, AuthenticationError } = require("apollo-server");
+const { ApolloServer, AuthenticationError } = require('apollo-server');
 
 // GraphQL imports
-const typeDefs = fs.readFileSync(path.join(__dirname, "graphql", "typeDefs.gql"), "utf-8");
-const resolvers = require("./graphql/resolvers/index");
+const typeDefs = fs.readFileSync(path.join(__dirname, 'graphql', 'typeDefs.gql'), 'utf-8');
+const resolvers = require('./graphql/resolvers/index');
 
 // MongoDB imports
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Get current user on every request
 const getUser = async token => {
@@ -21,44 +21,16 @@ const getUser = async token => {
 		try {
 			return await jwt.verify(token, process.env.SECRET);
 		} catch (err) {
-<<<<<<< HEAD
-			throw new Error('Your session has ended. Please sign in again.');
-=======
-			throw new AuthenticationError("Your session has ended. Please sign in again.");
->>>>>>> parent of e1e566c... Use Fastify instead of Apollo
+			throw new AuthenticationError('Your session has ended. Please sign in again.');
 		}
 	}
 };
 
-<<<<<<< HEAD
-// const server = new ApolloServer({
-// 	typeDefs,
-// 	resolvers,
-// 	context: async ({ req }) => {
-// 		const token = req.headers.authorization || "";
-// 		const currentUser = await getUser(token);
-// 		return {
-// 			currentUser
-// 		};
-// 	}
-// });
-
-fastify.register(require('fastify-cors'), {
-	origin: 'https://the-strength-log.netlify.com/',
-	credentials: true
-});
-
-fastify.register(gql, {
-	// jit: 1,
-	graphiql: 'playground',
-	schema: typeDefs,
-=======
 const server = new ApolloServer({
 	typeDefs,
->>>>>>> parent of e1e566c... Use Fastify instead of Apollo
 	resolvers,
 	context: async ({ req }) => {
-		const token = req.headers.authorization || "";
+		const token = req.headers.authorization || '';
 		const currentUser = await getUser(token);
 		return {
 			currentUser
@@ -74,10 +46,10 @@ mongoose
 		useFindAndModify: false
 	})
 	.then(() => {
-		console.log("DB connected");
+		console.log('DB connected');
 		return server.listen({ port: process.env.PORT || 4000 });
 	})
 	.then(({ url }) => {
-		console.log("Server listening on " + url);
+		console.log('Server listening on ' + url);
 	})
 	.catch(err => console.log(err));
