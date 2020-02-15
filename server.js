d6c65d35748b7aec53c2d83e4c9d2e646e13c075
Felix <felix_guerin@hotmail.fr>
2020-02-14 22:36:44 -0500

    Fix error message

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const getUser = async token => {
 		try {
 			return await jwt.verify(token, process.env.SECRET);
 		} catch (err) {
-			throw new AuthenticationError('Your session has ended. Please sign in again.');
+			throw new Error('Your session has ended. Please sign in again.');
 		}
 	}
 };
