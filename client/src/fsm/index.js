e0ebea09e5ee05d3bd0f9c525cf6c41c2bc31d5d
Felix <felix_guerin@hotmail.fr>
2020-02-17 21:36:40 -0500

    Update vue-xstate-plugin

--- client/src/fsm/index.js
+++
@@ -1,5 +0,0 @@
-import { authMachine } from "./authMachine";
-import { searchMachine } from "./searchMachine";
-import { sessionMachine } from "./sessionMachine";
-
-export const machines = [authMachine, searchMachine, sessionMachine];
--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,9 @@ import './registerServiceWorker';
 import router from './router';
 import { VueStateMachine } from 'vue-xstate-plugin';
 import { machines } from './fsm';
+import { authMachine } from './fsm/authMachine';
+import { searchMachine } from './fsm/searchMachine';
+import { sessionMachine } from './fsm/sessionMachine';
 
 // Global styles
 import { MdButton, MdIcon } from 'vue-material/dist/components';
@@ -17,9 +20,26 @@ Vue.config.productionTip = false;
 
 Vue.use(MdButton);
 Vue.use(MdIcon);
-Vue.use(VueStateMachine, {
-	machines
-});
+Vue.use(VueStateMachine, [
+	{
+		config: authMachine,
+		logState: true,
+		logContext: true,
+		persistState: true
+	},
+	{
+		config: searchMachine,
+		logState: true,
+		logContext: true,
+		persistState: true
+	},
+	{
+		config: sessionMachine,
+		logState: true,
+		logContext: true,
+		persistState: true
+	}
+]);
 
 new Vue({
 	router,
